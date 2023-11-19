684071b47604b07873161efe2ad0d838b8ef2044
daerias <n30phyt3.2007@gmail.com>
2023-11-19 16:57:32 +0100

    Create rename-GPX-inside-from-filename.py

---
+++ rename-GPX-inside-from-filename.py
@@ -0,0 +1,43 @@
+# Dateiname: rename_gpx-filenames_inside-GPX.py
+
+import os
+import glob
+import shutil
+from xml.etree import ElementTree as ET
+
+def rename_gpx_files():
+    path = './GPX/*.gpx'
+    files = glob.glob(path)
+
+    # Erstelle die Ordner "done" und "fail", wenn sie noch nicht existieren
+    if not os.path.exists('./done'):
+        os.makedirs('./done')
+    if not os.path.exists('./fail'):
+        os.makedirs('./fail')
+
+    for file in files:
+        try:
+            tree = ET.parse(file)
+            root = tree.getroot()
+
+            # Get filename without extension
+            filename = os.path.splitext(os.path.basename(file))[0]
+
+            # Replace content between <name> and </name> tags, regardless of namespace
+            for elem in root.iter():
+                if elem.tag.endswith('name'):
+                    elem.text = filename
+
+            # Write changes back to the file
+            tree.write(file)
+
+            # Verschiebe die erfolgreich umbenannte Datei in den "done"-Ordner
+            shutil.move(file, './done/' + os.path.basename(file))
+        except Exception as e:
+            print(f"Failed to rename {file} - Error: {str(e)}")
+
+            # Verschiebe die nicht erfolgreich umbenannte Datei in den "fail"-Ordner
+            shutil.move(file, './fail/' + os.path.basename(file))
+
+if __name__ == "__main__":
+    rename_gpx_files()
